d8e3463170d677b94643906096a3ed39de24d9a0
Sidharth Menon <sidharth.menon91@gmail.com>
2019-07-31 14:34:16 +0530

    nav resp

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,16 @@
-import $ from "jquery";
+import jQuery from "jquery";
 import popper from "popper.js";
-import "bootstrap";
+import bootstrap from "bootstrap";
 import SmoothScroll from 'smooth-scroll';
 
+window.$ = window.jQuery = jQuery;
+window.bootstrap = bootstrap;
+window.popper = popper;
+
 var scroll = new SmoothScroll('a[data-scroll]');
+
+$( '.hamburger-menu' ).on( 'click', function() {
+  $(this).toggleClass('open');
+  $('.site-navigation').toggleClass('show');
+});
 
